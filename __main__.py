b2d256a0923534ea7207a7c8b44555d26fa27197
saihnii4 <81787757+dulguuncodes@users.noreply.github.com>
2024-07-22 14:21:42 +0800

    cursor tag, etc.

--- __main__.py
+++ __main__.py
@@ -14,75 +14,107 @@ class App(tk.Frame):
 
 
 class Game(tk.Frame):
-    lines = []
     headstart_count = 3
     snake_pos = [1, 0]
     line_no = 1
+    _comparator = """The Woodward–Hoffmann rules are a set of rules devised by Robert
+        Burns Woodward and Roald Hoffmann to rationalize or predict certain aspects of the stereochemistry and activation energy of pericyclic"""
+    source_lines = _comparator.split("\n")
+    lines = [[] for _ in range(len(_comparator))]
 
     def __init__(self, master=None):
         super().__init__(master)
-
-        self._comparator = """The Woodward–Hoffmann rules are a set of rules devised by Robert
-        Burns Woodward and Roald Hoffmann to rationalize or predict certain aspects of the stereochemistry and activation energy of pericyclic"""
-
-        self.source_lines = self._comparator.split("\n")
-
-        self.canvas = tk.Canvas(self, width=600, height=500)
-        self.canvas.pack()
-        self.canvas.bind_all("<Key>", self.handle_keypress)
-
-        self.view = tk.Text(self, background="orange")
-        self.view.insert(tk.INSERT, self.comparator)
-
-        self.view.after(1000, self.loop)
-
-        self.view.tag_configure("eaten", foreground="black", background="green")
-        self.view.tag_configure("err", foreground="blue", background="red")
-        self.view.tag_configure("inactive", foreground="gray", background="orange")
-        self.view.tag_configure("active", foreground="black", background="orange")
-
-        self.view.tag_raise("active", "inactive")
-        self.view.tag_raise("err", "active")
-        self.view.tag_raise("eaten", "err")
-
-        self.view.tag_add("inactive", format_pos(1, 0), tk.END)
-        self.view.configure(state="disabled")
-        self.canvas.create_window(300, 300, window=self.view)
-
         self.pack()
+
+        self._canvas = tk.Canvas(self, width=600, height=500)
+        self._canvas.pack(side='bottom')
+        self._canvas.bind_all("<Key>", self.handle_keypress)
+
+        self._view = tk.Text(self, background="white")
+        self._view.insert(tk.INSERT, self._comparator)
+
+        self.init()
+
+    def init(self):
+        # configure tags
+        self._view.tag_configure("eaten", foreground="black", background="green")
+        self._view.tag_configure("err", foreground="black", background="red")
+        self._view.tag_configure("inactive", foreground="gray", background="white")
+        self._view.tag_configure("active", foreground="black", background="white")
+        self._view.tag_configure("cursor", underline=True, underlinefg="black")
+        # tag hierarchy
+        self._view.tag_raise("active", "inactive")
+        self._view.tag_raise("err", "active")
+        self._view.tag_raise("cursor", "active")
+        self._view.tag_raise("eaten", "err")
+        # deactivate all chars
+        self._view.tag_add("inactive", format_pos(1, 0), tk.END)
+        self._view.configure(state="disabled")
+        # create canvas
+        self._canvas.create_window(300, 300, window=self._view)
+        # start snake loop
+        self._view.after(1000, self.loop)
 
     def _line_ind(self):
         return self.line_no - 1
 
     line_ind = property(_line_ind)
 
+    def _text(self):
+        return self.lines[self.line_ind]
+
+    text = property(_text)
+
+    def _pos(self):
+        return (self.line_no, len(self.lines[self.line_ind]))
+
+    pos = property(_pos)
+
     def loop(self):
         if self.headstart_count:
             self.headstart_count -= 1
-            self.view.after(1000, self.test)
+            self._view.after(1000, self.loop)
             return
 
         self.snake_pos[1] += 1
-        if self.snake_pos[1] > len(self.lines[self.snake_pos[0]]):
+        if self.snake_pos[1] > len(self.source_lines[self.snake_pos[0] - 1]):
             self.snake_pos[0] += 1
 
+        if self.snake_pos[1] > len(self.lines[self.snake_pos[0] - 1]):
+            # TODO:
+            print("CAUGHT UP")
+            self._canvas.unbind_all("<Key>")
+            return
+
         self.render_text()
-        self.view.after(1000, self.loop)
+        self._view.after(1000, self.loop)
 
     def render_text(self):
-        self.view.tag_add("inactive", format_pos(1, 0), tk.END)
-        self.view.tag_remove("err", format_pos(1, 0), tk.END)
-        self.view.tag_remove(
+        print(self.pos)
+        # flush...
+        self._view.tag_add("inactive", format_pos(1, 0), tk.END)
+        self._view.tag_remove("err", format_pos(1, 0), tk.END)
+        self._view.tag_remove("cursor", format_pos(1,0), tk.END)
+        self._view.tag_remove("active", format_pos(1, 0), tk.END)
+        self._view.tag_remove(
             "inactive", format_pos(1, 0), format_pos(self.line_no, len(self.text))
         )
-        self.view.tag_add("eaten", format_pos(1, 0), format_pos(*self.snake_pos))
+        # then paint
+        self._view.tag_add(
+            "active", format_pos(1, 0), format_pos(self.line_no, len(self.text))
+        )
+        self._view.tag_add(
+            "cursor", format_pos(*self.pos), format_pos(*(self.pos[0], self.pos[1]+1))
+        )
+        self._view.tag_add("eaten", format_pos(1, 0), format_pos(*self.snake_pos))
 
-        for i, val in enumerate(self.correct(self.lines[self.line_no])):
+        for i, val in enumerate(self.correct(self.text)):
             if val:
                 continue
-            self.view.tag_add("err", format_pos(1, i), format_pos(self.line_no, i + 1))
+            self._view.tag_add("err", format_pos(self.line_no, i), format_pos(self.line_no, i + 1))
 
     def handle_keypress(self, ev: tk.Event):
+        # TODO: handle XP ascii keycodes
         if KEYMAP.get(ev.keycode) is None:
             return
 
@@ -90,20 +122,20 @@ class Game(tk.Frame):
         if isinstance(ref, SpecialKey):
             if ref == SpecialKey.BACKSPACE and len(self.text) > 0:
                 del self.text[-1]
-            if ref == SpecialKey.ENTER:
+            if ref == SpecialKey.ENTER and len(self.text) >= len(self.source_lines[self.line_ind]):
                 self.line_no += 1
                 print("TODO")
             self.render_text()
             return
 
-        if len(self.lines[self.line_no]) < len(self.source_lines[self.line_no]):
+        if len(self.lines[self.line_ind]) < len(self.source_lines[self.line_ind]):
             self.text.append(KEYMAP.get(ev.keycode))
-        self.render_text()
+            self.render_text()
 
     def correct(self, value):
         for i, char in enumerate(value):
-            print(self.source_lines[self.line_no][i], char, i)
-            yield self.comparator[self.line_no][i] == char
+            print(self.source_lines[self.line_ind][i], char, i)
+            yield self.source_lines[self.line_ind][i] == char
 
 
 # class Test(tk.Frame):
@@ -145,8 +177,11 @@ class Game(tk.Frame):
 root = tk.Tk()
 # app = App()
 # app.pack(side='top', anchor='center', pady=100)
-game = Game()
-game.pack(side="top", anchor="center", pady=100)
+a = tk.Text(root, background="red", height=40, width=60)
+a.insert(tk.INSERT, "hai")
+a.pack(side='left')
+game = Game(root)
+game.pack(side="top", pady=100, expand=True)
 # test = MainScreen(app)
 # bar = Bar(test)
 # a = Test(test)
